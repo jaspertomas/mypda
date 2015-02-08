7931c51285fda8a2757ca9c2c4ebe22344b2d3a6
Jasper Tomas <jaspertomas@gmail.com>
2015-02-08 14:38:16 +0800

    db works

--- src/com/itforhumanity/mypda/MainActivity.java
+++ src/com/itforhumanity/mypda/MainActivity.java
@@ -1,7 +1,12 @@
 package com.itforhumanity.mypda;
 
+import java.util.Date;
 import java.util.Locale;
 
+import models.Input;
+import models.Inputs;
+import utils.DateTimeHelper;
+import utils.MyApplicationContextHolder;
 import android.app.ActionBar;
 import android.app.FragmentTransaction;
 import android.os.Bundle;
@@ -9,12 +14,10 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.app.NavUtils;
 import android.support.v4.view.ViewPager;
-import android.view.Gravity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -76,6 +79,9 @@ public class MainActivity extends FragmentActivity implements
 					.setText(mSectionsPagerAdapter.getPageTitle(i))
 					.setTabListener(this));
 		}
+		
+		MyApplicationContextHolder.setAppContext(getApplicationContext());
+		
 	}
 
 	@Override
